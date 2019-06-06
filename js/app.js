3486dbe164efb209804017aea6d037c2dd93e3b3
Lillian Gales <lilliangales@ymail.com>
2019-06-05 23:38:48 -0700

    key item option added

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function startApp(){
 
 // load the horm data from json file
 function loadData(){
-  
+
   const success = horns => {
     displayHorns(horns);
     displayKeywords(horns);
@@ -23,13 +23,13 @@ function loadData(){
   console.log('you got the data!!')
 
   //call the horn display function
-  
-  
+
+
 }
 
 // populate the horns on the page
 function displayHorns(horns){
-  //method on the array 
+  //method on the array
   horns.forEach(horn => {
     //assign clone to id
     const $newHorn = $('#photo-template').clone();
@@ -50,9 +50,22 @@ function displayHorns(horns){
 // populate the keywords
 
 function displayKeywords(horns) {
-  let keywords = [];
+  const keywords = [];
   horns.forEach(horn => {
-
+    if (!keywords.includes(horn.keyword)){
+      keywords.push(horn.keyword)
+    }
   });
-  console.log('should be doing display things')
+
+  keywords.forEach(keyword => {
+    const $newKeyword = $('#keyword-template').clone();
+    $newKeyword.text(keyword);
+    $newKeyword.removeAttr('#keyword-template');
+    $('select').append($newKeyword)
+    console.log('this is the keyword', keywords)
+  })
+
 }
+
+// select event listener
+
