4dbc1e763da530ad2d0b1c97d475e706b1e35802
Lillian Gales <lilliangales@ymail.com>
2019-06-09 23:51:22 -0700

    fixed keywords

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@
 // start up everything
 $(startApp);
 
-// global variables
-const keywords = []
 
 
 
@@ -51,17 +49,25 @@ function pageClickListeners(){
 
 
 function displaySelectedKeyword(horns) {
+  const keywords = []
   horns.forEach(horn => {
     if (!keywords.includes(horn.keyword)) {
       keywords.push(horn.keyword)
     }
   })
-  const template = Handlebars.compile($('#keyword-template').html());
-  // for each horn pass in horns to the template
-  const render = template({keywords: keywords });
-  $('#keyword-section').html(render);
 
+  const keywordElements = [];
 
+  const render = Handlebars.compile($('#keyword-template').html());
+  keywords.forEach(keywordVal => {
+    console.log(render({keyword: keywordVal}))
+    console.log({keyword: keywordVal})
+    keywordElements.push(render({keyword: keywordVal}));
+  })
+  
+  console.log('should be displaying keywords')
+  $('.new-keywords').remove();
+  $('#keyword-section').append(keywordElements);
 }
 
 function attachSelectListeners() {
